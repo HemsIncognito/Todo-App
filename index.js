a9c19b53d21895498d9afd92fefb53f6bd20cfd7
JackHarperIMF <hemvayyala8@gmail.com>
2024-04-25 16:59:05 +0530

    cors

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const dbConnection = require("./dbConnection");
 const apis = require('./routes/route');
 const env = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 env.config();
 
 const app = express();
 app.use(express.json()); //parse the json 
+
+app.use(cors()); //use cors to connect across
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = 8081;
 
