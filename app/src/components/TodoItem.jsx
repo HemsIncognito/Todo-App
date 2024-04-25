fed0a490f44f5c87955f3f34c05569a8d1f412c1
JackHarperIMF <hemvayyala8@gmail.com>
2024-04-25 17:47:44 +0530

    icons_visibility

--- app/src/components/TodoItem.jsx
+++ app/src/components/TodoItem.jsx
@@ -16,23 +16,25 @@ const Todo = ({ todo }) => {
     }
 
     return (
-        <li className="task" onClick={() => dispatch(toggleDone(todo._id))}
-            style={{
-                textDecoration: todo?.done ? 'line-through' : '',
-                color: todo?.done ? '#bdc3c7' : '#34495e'
-            }}
-            data-testid="todo-test"
-        >
-            <span style={{ display: editing ? 'none' : '' }}>{todo?.data}</span>
+        <li className="task"
+        style={{
+            textDecoration: todo?.done ? 'line-through' : '',
+            color: todo?.done ? '#bdc3c7' : '#34495e'
+        }}>
+            <span style={{ display: editing ? 'none' : '' }}
+            onClick={() => dispatch(toggleDone(todo._id))}> 
+                {todo?.data}
+            </span>
 
-            <form style={{ display: editing ? 'inline' : 'none' }} onSubmit={onFormSubmit}>
-							<input type="text" value={text} className="edit-todo" onChange={(e) => setText(e.target.value)}/>
+            <form style={{ display: editing ? 'inline' : 'none' }} 
+            onSubmit={onFormSubmit}>
+				<input type="text" value={text} className="edit-todo" onChange={(e) => setText(e.target.value)}/>
             </form>
 
             <span className="icon" onClick={() => dispatch(deleteTodo(todo._id))}>
                 <i className="fas fa-trash" />
             </span>
-            <span className="icon" onClick={() => setEditing(prevState => !prevState)}>
+            <span className="icon" onClick={() => setEditing(true)}>
                 <i className="fas fa-pen" />
             </span>
         </li>
